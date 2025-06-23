878aa4712382f400386979f405746fe7c7c8c2fc
I-EN LEE <ian10301004@gmail.com>
2025-06-23 21:19:13 +0200

    Update version 2.1.0 & 2.2.0

---
+++ version_2.2.0/version_2.2.0.ino/humidity.h
@@ -0,0 +1,17 @@
+#ifndef HUMIDITY_IMAGE_H
+#define HUMIDITY_IMAGE_H
+
+#include "lvgl.h"
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+    extern const lv_img_dsc_t humidity;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // HUMIDITY_IMAGE_H
